56256889f5286268816cc64e4db72c371b120cfb
ALXK <98492373+alxk0@users.noreply.github.com>
2023-05-26 06:21:42 -0400

    ml

--- automata/js/load.js
+++ automata/js/load.js
@@ -5,7 +5,28 @@ $(document).ready(function() {
     "txt/001.txt",
     "txt/002.txt",
     "txt/003.txt",
-    "txt/004.txt"
+    "txt/004.txt",
+    "txt/005.txt",
+    "txt/006.txt",
+    "txt/007.txt",
+    "txt/008.txt",
+    "txt/009.txt",
+    "txt/010.txt",
+    "txt/011.txt",
+    "txt/012.txt",
+    "txt/013.txt",
+    "txt/014.txt",
+    "txt/015.txt",
+    "txt/016.txt",
+    "txt/017.txt",
+    "txt/018.txt",
+    "txt/019.txt",
+    "txt/020.txt",
+    "txt/021.txt",
+    "txt/022.txt",
+    "txt/023.txt",
+    "txt/024.txt",
+    "txt/025.txt"
   ];
 
   // Function to update the content
