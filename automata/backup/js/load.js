c468b9f95f241d2e67c2355805ab0fb028d67ff6
ALXK <98492373+alxk0@users.noreply.github.com>
2023-05-26 05:43:41 -0400

    ml

---
+++ automata/backup/js/load.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var contentIndex = 0;
+  var contents = [
+    "txt/000.txt",
+    "txt/001.txt",
+    "txt/002.txt",
+    "txt/003.txt",
+    "txt/004.txt"
+  ];
+
+  // Function to update the content
+  function updateContent() {
+    var fileName = contents[contentIndex];
+    $.ajax({
+      url: fileName,
+      dataType: "text",
+      success: function(data) {
+        $('.content00').html(data);
+      },
+      error: function() {
+        $('.content00').html("<p>Error loading content.</p>");
+      }
+    });
+  }
+
+  // Initial content update
+  updateContent();
+
+  // "NEXT" link click event handler
+  $('#link1').click(function(e) {
+    e.preventDefault();
+    contentIndex++;
+    if (contentIndex >= contents.length) {
+      contentIndex = 0;
+    }
+    updateContent();
+  });
+});
